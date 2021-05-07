61ce5a82a539f29e92c41d60e52253e2369f4411
Sebastian <denzilbao@gmail.com>
2021-05-07 11:35:46 +0800

    backend setup.

--- web-client/src/plugins/vuetify.js
+++ web-client/src/plugins/vuetify.js
@@ -1,7 +1,15 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#0B6B09",
+        accent: "#F5C509",
+      },
+    },
+  },
 });
---
+++ web-client/src/router/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-app-bar app flat color="primary" dark>
+      <v-container>
+        <v-toolbar-title>
+          <v-avatar :size="40">
+            <v-img :src="require('@/assets/udm-logo.png')"></v-img>
+          </v-avatar>
+          <span class="font-weight-bold ml-3">UDM</span>
+        </v-toolbar-title>
+      </v-container>
+    </v-app-bar>
+    <v-main>
+      <h1>Test</h1>
+    </v-main>
+  </v-app>
+</template>
